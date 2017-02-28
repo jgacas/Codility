3b6f43d0d808a26d825edb1fbad8d1434c5cf458
jgacas <jugoslav.gacas@gmail.com>
2017-02-28 21:40:23 +0100

    Convert tabs to spaces.

--- src/binarygap/SolutionTest.java
+++ src/binarygap/SolutionTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 
 public class SolutionTest {
 
-	@Test
-	public void testSolution() {
-		Solution binaryGap = new Solution();
-		
-		assertEquals(2, binaryGap.solution(9));
-		assertEquals(4, binaryGap.solution(529));
-		assertEquals(1, binaryGap.solution(20));
-	}
+    @Test
+    public void testSolution() {
+        Solution binaryGap = new Solution();
+
+        assertEquals(2, binaryGap.solution(9));
+        assertEquals(4, binaryGap.solution(529));
+        assertEquals(1, binaryGap.solution(20));
+    }
 
 }
--- src/oddoccurrences/SolutionTest.java
+++ src/oddoccurrences/SolutionTest.java
@@ -5,40 +5,37 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SolutionTest {
-	
-	@Test
-	public void testSolutionWhenFirstElementIsOddOccurrence()
-	{
-		Solution oddOccurrences = new Solution();
-		
-		int input[] = {3, 9, 5, 9, 5};
-		assertEquals(3, oddOccurrences.solution(input, input.length));
-	}
-	
-	@Test
-	public void testSolutionWhenSecondElementIsOddOccurrence()
-	{
-		Solution oddOccurrences = new Solution();
-		
-		int input[] = {9, 3, 9};
-		assertEquals(3, oddOccurrences.solution(input, input.length));
-	}
 
-	@Test
-	public void testSolutionWhenElementBeforeLastOneIsOddOccurrence()
-	{
-		Solution oddOccurrences = new Solution();
-		
-		int input[] = {9, 3, 9, 3, 9, 7, 9};
-		assertEquals(7, oddOccurrences.solution(input, input.length));
-		
-	}
-	
-	@Test
-	public void testSoultionWhenLastElementIsOddOccurrence() {
-		Solution oddOccurrences = new Solution();
-		
-		int input[] = {9, 3, 9, 3, 5};
-		assertEquals(5, oddOccurrences.solution(input, input.length));
-	}
+    @Test
+    public void testSolutionWhenFirstElementIsOddOccurrence() {
+        Solution oddOccurrences = new Solution();
+
+        int input[] = { 3, 9, 5, 9, 5 };
+        assertEquals(3, oddOccurrences.solution(input, input.length));
+    }
+
+    @Test
+    public void testSolutionWhenSecondElementIsOddOccurrence() {
+        Solution oddOccurrences = new Solution();
+
+        int input[] = { 9, 3, 9 };
+        assertEquals(3, oddOccurrences.solution(input, input.length));
+    }
+
+    @Test
+    public void testSolutionWhenElementBeforeLastOneIsOddOccurrence() {
+        Solution oddOccurrences = new Solution();
+
+        int input[] = { 9, 3, 9, 3, 9, 7, 9 };
+        assertEquals(7, oddOccurrences.solution(input, input.length));
+
+    }
+
+    @Test
+    public void testSoultionWhenLastElementIsOddOccurrence() {
+        Solution oddOccurrences = new Solution();
+
+        int input[] = { 9, 3, 9, 3, 5 };
+        assertEquals(5, oddOccurrences.solution(input, input.length));
+    }
 }
